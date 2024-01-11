5f99c273526f570cc42e48a3ba4702cc04e25e05
Dakota Dudley <dakotadudley13@gmail.com>
2024-01-11 13:11:11 -0500

    feat(markers) updated marker and integrated with map

--- natural-disaster-tracker/src/components/WildfireMarker.tsx
+++ natural-disaster-tracker/src/components/WildfireMarker.tsx
@@ -1,17 +1,53 @@
 import { Icon } from "@iconify/react"
+import { PureComponent, useState } from "react";
+import { Marker } from "google-maps-react"
+import LocationInfoBox from "./LocationInfoBox";
 
 interface WildfireProps {
-    lat: number,
-    lng: number,
-    onClick: React.MouseEventHandler<HTMLDivElement>;
+    id: string,
+    google: any,
+    stats: Array<{ lat: number; lng: number; id: string, title: string}>;
+    mouseEnterHandler: () => void;
+    mouseLeaveHandler: () => void;
 }
 
-const WildfireMarker: React.FC<Partial<WildfireProps>> = ({ lat, lng, onClick}) => {
-  return (
-    <div className="hover:cursor-pointer" onClick={onClick}>
-        <Icon icon="emojione:fire" className="text-4xl text-red-500" />
-    </div>
-  )
+const WildfireMarker: React.FC<WildfireProps> = ({ id, google, stats, mouseEnterHandler, mouseLeaveHandler, ...props }) => {
+  const [locationInfo, setLocationInfo] = useState<{id: string, title: string} | null>()
+  let iw = 70,
+    ih = 94;
+
+  // Add a check for stats to avoid the "Cannot read properties of undefined" error
+  if (!stats) {
+    return null; // or you can return some default content or loading indicator
+  }
+
+
+  return stats.map((statObj) => {
+      console.log('logging')
+
+      const infoObject = {
+        key: id,
+        position: {lat: statObj.lat, lng: statObj.lng},
+        onClick: () => {
+          setLocationInfo({ id: statObj.id, title: statObj.title });
+        },
+        onMouseover: mouseEnterHandler,
+        onMouseout: mouseLeaveHandler,
+        google: google,
+        ...props,
+        icon: {
+          url: `https://waqi.info/mapicon/1.30.png`,
+          anchor: new google.maps.Point(iw / 4, ih / 2 - 5),
+          size: new google.maps.Size(iw / 2, ih / 2),
+          scaledSize: new google.maps.Size(iw / 2, ih / 2),
+        },
+      };
+
+      return (
+        <Marker {...infoObject} />
+      );
+  });
+
 }
 
 export default WildfireMarker
