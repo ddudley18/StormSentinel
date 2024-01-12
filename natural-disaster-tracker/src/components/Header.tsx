7dec3ffa35a98c880cc2f17326993ecf6105a219
Dakota Dudley <dakotadudley13@gmail.com>
2024-01-12 16:34:59 -0500

    feat(header) header styling

--- natural-disaster-tracker/src/components/Header.tsx
+++ natural-disaster-tracker/src/components/Header.tsx
@@ -2,12 +2,12 @@ import { Icon } from "@iconify/react"
 
 const Header = () => {
   return (
-    <header className="p-2 bg-blue-800 text-white fixed top-0 right-0 left-0 z-50 border-b-4 border-solid border-white">
-        <h1 className="text-3xl pt-0 mt-0 text-center">
-            Severe Weather Tracker (Powered By <Icon className="inline pb-1" icon="simple-icons:nasa" width="84" height="84"/>)
+    <header className="p-1 bg-nasa-blue text-white fixed top-0 right-0 left-0 z-50 border-b-2 border-solid border-white overflow-hidden whitespace-nowrap">
+        <h1 className="text-4xl pt-0 mt-0 text-center">
+            Severe Weather Tracker (Powered By <Icon className="inline pb-1 animate-pulse" icon="simple-icons:nasa" width="100" height="100"/> <img src="../satelliteOne.gif"  className="w-14 h-14 inline" alt="GIF"></img>)
         </h1>
     </header>
   )
 }
 
-export default Header
+export default Header;
--- natural-disaster-tracker/src/pages/_app.tsx
+++ natural-disaster-tracker/src/pages/_app.tsx
@@ -1,13 +1,22 @@
 import "../app/globals.css";
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
 
 import { Provider } from "react-redux";
 import store from "@/store";
  
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <>
+      <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Blinker&display=swap" rel="stylesheet"/>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+      </Head>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </>
   );
 }
